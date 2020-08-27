0a93a7d79e5d52ef3d364d800d0ba2373eaa0a7a
kazenetu <rakudafactory@gmail.com>
2020-08-27 23:54:58 +0900

    [WIP]Activity間の画面遷移エフェクト設定

--- MultiActivity/app/src/main/java/com/github/kazenetu/multiactivity/FirstActivity.kt
+++ MultiActivity/app/src/main/java/com/github/kazenetu/multiactivity/FirstActivity.kt
@@ -1,8 +1,11 @@
 package com.github.kazenetu.multiactivity
 
+import android.app.ActivityOptions
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.transition.Explode
+import android.view.Window.*
 import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
@@ -12,11 +15,17 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
 
+        with(window) {
+            // set transition
+            enterTransition = Explode()
+            exitTransition = Explode()
+        }
+
         button.setOnClickListener { view ->
             val intent = Intent(this, SecondActivity::class.java).apply {
                 //putExtra(EXTRA_MESSAGE, message)
             }
-            startActivity(intent)
+            startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
     }
--- MultiActivity/app/src/main/java/com/github/kazenetu/multiactivity/SecondActivity.kt
+++ MultiActivity/app/src/main/java/com/github/kazenetu/multiactivity/SecondActivity.kt
@@ -2,10 +2,18 @@ package com.github.kazenetu.multiactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Explode
+import android.view.Window
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        with(window) {
+            // set transition
+            enterTransition = Explode()
+            exitTransition = Explode()
+        }
     }
 }
