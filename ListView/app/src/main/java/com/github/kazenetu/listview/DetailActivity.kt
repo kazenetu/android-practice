d5312b0c4c170d856d8698adf3025ee42e1cf4f4
kazenetu <rakudafactory@gmail.com>
2020-09-20 22:38:14 +0900

    詳細ページにエフェクトを追加

--- ListView/app/src/main/java/com/github/kazenetu/listview/DetailActivity.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/DetailActivity.kt
@@ -8,4 +8,23 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
     }
+
+    /**
+     * 親Activityに戻るイベント
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        // Backを実行
+        onBackPressed()
+        return true
+    }
+
+    /**
+     * 戻るボタン
+     */
+    override fun onBackPressed()
+    {
+        super.onBackPressed()
+        overridePendingTransition( R.anim.detail_in,R.anim.detail_out)
+    }
+
 }
