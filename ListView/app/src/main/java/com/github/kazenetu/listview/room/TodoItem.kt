98a0f44f7cdaaf1b78e16de390cdf621ff5756fd
kazenetu <rakudafactory@gmail.com>
2020-10-17 22:38:36 +0900

    Room用Entityクラスを追加

---
+++ ListView/app/src/main/java/com/github/kazenetu/listview/room/TodoItem.kt
@@ -0,0 +1,16 @@
+package com.github.kazenetu.listview.room
+
+import androidx.room.*
+
+/**
+ * リストアイテム
+ */
+@Entity(tableName = "items")
+data class TodoItem(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @Ignore var showImage: Boolean,
+    var title: String,
+    var detail: String
+){
+    constructor(id:Int,title: String,detail: String):this(id,false,title,detail)
+}
