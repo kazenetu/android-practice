6bb1c8a63a9c2cd2fa030c8808697b486bca2ae8
kazenetu <rakudafactory@gmail.com>
2020-10-27 21:42:44 +0900

    koinコンテナ生成

--- ListView/app/src/main/java/com/github/kazenetu/listview/App.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/App.kt
@@ -1,5 +1,9 @@
 package com.github.kazenetu.listview
 
+import org.koin.dsl.module.Module
+import org.koin.dsl.module.module
+import org.koin.android.ext.android.startKoin
+
 /**
  * アプリケーションクラス
  */
@@ -7,5 +11,15 @@ class App :android.app.Application()
 {
     override fun onCreate() {
         super.onCreate()
+
+        // Koinコンテナ生成
+        startKoin(this, listOf(
+            this.modules
+        ))
+    }
+
+    // Koinモジュール
+    private val modules: Module = module {
+        factory { TodoViewModel(this@App) }
     }
 }
