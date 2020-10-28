73f27a80bb8e5225b1a1bada6386f77060e8538a
kazenetu <rakudafactory@gmail.com>
2020-10-28 22:03:30 +0900

    TodoRepositoryクラスをDIコンテナ対象に追加

--- ListView/app/src/main/java/com/github/kazenetu/listview/App.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/App.kt
@@ -1,5 +1,7 @@
 package com.github.kazenetu.listview
 
+import com.github.kazenetu.listview.repository.TodoRepository
+import com.github.kazenetu.listview.room.AppDatabase
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.module
 import org.koin.android.ext.android.startKoin
@@ -7,19 +9,21 @@ import org.koin.android.ext.android.startKoin
 /**
  * アプリケーションクラス
  */
-class App :android.app.Application()
-{
+class App :android.app.Application() {
     override fun onCreate() {
         super.onCreate()
 
         // Koinコンテナ生成
-        startKoin(this, listOf(
-            this.modules
-        ))
+        startKoin(
+            this, listOf(
+                this.modules
+            )
+        )
     }
 
     // Koinモジュール
     private val modules: Module = module {
-        factory { TodoViewModel(this@App) }
+        factory { TodoRepository(AppDatabase.getDatabase(this@App).todoDao()) }
+        factory { TodoViewModel(this@App, get()) }
     }
 }
