194da2d9501e873c7193a5884aabcf5440a0871a
kazenetu <rakudafactory@gmail.com>
2020-10-29 22:30:24 +0900

    Databaseサブクラスのシングルトンを解除

--- ListView/app/src/main/java/com/github/kazenetu/listview/room/AppDatabase.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/room/AppDatabase.kt
@@ -1,37 +1,9 @@
 package com.github.kazenetu.listview.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.github.kazenetu.listview.room.migrations.Migration1to2
 
 @Database(entities = arrayOf(TodoItem::class), version = 2, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun todoDao(): TodoDao
-
-    companion object {
-        // Singleton prevents multiple instances of database opening at the
-        // same time.
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "todo_database.db"
-                )
-                .addMigrations(Migration1to2.migration)
-                .build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
 }
