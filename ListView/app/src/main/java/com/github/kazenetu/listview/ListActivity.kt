db96f194b114e698c8f4ca0ff984a9602fa3dfca
kazenetu <rakudafactory@gmail.com>
2020-10-29 22:35:57 +0900

    不要なimportを削除

--- ListView/app/src/main/java/com/github/kazenetu/listview/ListActivity.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/ListActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.kazenetu.listview.room.TodoItem
