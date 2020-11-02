6df9784733dc116e97f3cad30307db95d953638a
kazenetu <rakudafactory@gmail.com>
2020-11-03 08:36:30 +0900

    リストアイテムの要素追加

--- ListView/app/src/main/java/com/github/kazenetu/listview/ItemViewHolder.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/ItemViewHolder.kt
@@ -1,7 +1,9 @@
 package com.github.kazenetu.listview
 
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.text_row_item.view.*
@@ -11,6 +13,8 @@ import kotlinx.android.synthetic.main.text_row_item.view.*
  */
 class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
     val deleteTarget: ImageView = itemView.delete_target
+    val liner:LinearLayout =  itemView.linear
     val titleView: TextView = itemView.row_title
     val detailView: TextView = itemView.row_detail
+    var donekButton: ImageButton = itemView.donekButton
 }
