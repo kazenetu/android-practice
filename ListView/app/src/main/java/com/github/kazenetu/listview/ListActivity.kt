626e94f54ce98f2ecd12a3ffd15d2dadbaa1ca5b
kazenetu <rakudafactory@gmail.com>
2020-11-18 22:30:42 +0900

    不要なimportを削除

--- ListView/app/src/main/java/com/github/kazenetu/listview/ListActivity.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/ListActivity.kt
@@ -1,24 +1,9 @@
 package com.github.kazenetu.listview
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.ListFragment
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.github.kazenetu.listview.fragments.BlankFragment
-import com.github.kazenetu.listview.fragments.LFragment
 import com.github.kazenetu.listview.fragments.TodoFragment
-import com.github.kazenetu.listview.room.TodoItem
-import com.google.android.material.floatingactionbutton.*
-import kotlinx.android.synthetic.main.activity_list.*
-import org.koin.android.ext.android.inject
-import org.koin.androidx.viewmodel.ext.android.viewModel
-
 
 /**
  * メインActivity
