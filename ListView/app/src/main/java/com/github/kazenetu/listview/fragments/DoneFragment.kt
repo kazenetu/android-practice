6ced0003b9cd2ed740f1f1a58f4dc38a5b376e0c
kazenetu <rakudafactory@gmail.com>
2020-11-26 22:13:23 +0900

    Done用Fragmentを実装

--- ListView/app/src/main/java/com/github/kazenetu/listview/fragments/DoneFragment.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/fragments/DoneFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.github.kazenetu.listview.R
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.kazenetu.listview.*
+import com.github.kazenetu.listview.room.TodoItem
+import org.koin.android.ext.android.inject
 
 /**
  * A simple [Fragment] subclass.
@@ -14,8 +20,34 @@ import com.github.kazenetu.listview.R
  */
 class DoneFragment : Fragment() {
 
+    /**
+     * リストビューのインスタンス
+     */
+    private lateinit var recyclerView: RecyclerView
+
+    /**
+     * TodoViewModelのインスタンス
+     */
+    private val doneViewModel: DoneViewModel by inject()
+
+    /**
+     * RecyclerView.Adapterのインスタンス
+     */
+    private lateinit var adapter: ViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // ViewModelの更新監視
+        doneViewModel.listItems.observe(this, Observer {
+            it.let{adapter.setList(it)}
+        })
+        doneViewModel.update.observe(this, Observer { index ->
+            adapter.notifyItemChanged(index)
+        })
+        doneViewModel.delete.observe(this, Observer { index ->
+            adapter.notifyItemRemoved(index)
+        })
     }
 
     override fun onCreateView(
@@ -23,7 +55,45 @@ class DoneFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_done, container, false)
+        var view = inflater.inflate(R.layout.fragment_done, container, false)
+
+        recyclerView = view.findViewById(R.id.recycler_list)
+
+        // リストセット
+        adapter = ViewAdapter(activity?.applicationContext!!, object:ViewAdapter.ItemClickListener {
+            /**
+             * アイテムクリックイベント
+             */
+            override fun onItemClick(view: View, position: Int, value: TodoItem) {
+            }
+
+            /**
+             * アイテム長押し
+             */
+            override fun onItemLongClickListener(view: View, position: Int, value: TodoItem): Boolean {
+                return true
+            }
+
+            /**
+             * Doneボタン
+             */
+            override fun onItemDoneClick(view: View, position: Int, value: TodoItem) {
+                doneViewModel.update(position,
+                    RowItem(false,value.title,value.detail,!value.isDone)
+                )
+                adapter.notifyItemChanged(position)
+            }
+        })
+        // Adapterの内容がRecyclerViewのサイズに影響しない場合はtrueにするとパフォーマンスアップ
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context!!)
+        recyclerView.adapter = adapter
+
+        // 区切り線を設定
+        val separateLine = DividerItemDecoration(activity, DividerItemDecoration.VERTICAL)
+        recyclerView.addItemDecoration(separateLine)
+
+        return view
     }
 
     companion object {
