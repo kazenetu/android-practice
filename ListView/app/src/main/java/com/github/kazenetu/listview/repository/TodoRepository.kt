0c7aff213f5cebcdf0991cec3354c5e89141456f
kazenetu <rakudafactory@gmail.com>
2020-11-26 22:56:38 +0900

    Todo用とDone用のLiveDataを追加

--- ListView/app/src/main/java/com/github/kazenetu/listview/repository/TodoRepository.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/repository/TodoRepository.kt
@@ -6,6 +6,8 @@ import com.github.kazenetu.listview.room.TodoItem
 
 class TodoRepository(private val todoDao: TodoDao) {
     val allData: LiveData<List<TodoItem>> = todoDao.getAll()
+    val todoData: LiveData<List<TodoItem>> = todoDao.getTodo()
+    val doneData: LiveData<List<TodoItem>> = todoDao.getDone()
 
     suspend fun insert(todo: TodoItem) {
         todoDao.insert(todo)
