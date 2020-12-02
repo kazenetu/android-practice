d222480eda1acb5df52a056b8f6d39a67aefcb0f
kazenetu <rakudafactory@gmail.com>
2020-12-02 22:10:47 +0900

    SerializableからParcelableに変更

--- ListView/app/src/main/java/com/github/kazenetu/listview/RowItem.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/RowItem.kt
@@ -1,6 +1,10 @@
 package com.github.kazenetu.listview
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * リストアイテム
  */
-data class RowItem(var showImage:Boolean, var title:String, var detail:String,var isDone:Boolean):java.io.Serializable
+@Parcelize
+data class RowItem(var showImage:Boolean, var title:String, var detail:String,var isDone:Boolean):Parcelable
