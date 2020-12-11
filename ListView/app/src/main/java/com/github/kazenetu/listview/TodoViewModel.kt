c3bb39bd3c7c50a22d155e0b463a03773a921723
kazenetu <rakudafactory@gmail.com>
2020-12-11 22:08:34 +0900

    不要なimportを削除

--- ListView/app/src/main/java/com/github/kazenetu/listview/TodoViewModel.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/TodoViewModel.kt
@@ -1,12 +1,6 @@
 package com.github.kazenetu.listview
 
-import android.app.Application
-import androidx.lifecycle.*
 import com.github.kazenetu.listview.repository.TodoRepository
-import com.github.kazenetu.listview.room.TodoItem
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 /**
  * TODOリスト用ViewModel
