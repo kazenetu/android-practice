f489760b90549aaac79fb5eec8d9c58121226e98
kazenetu <rakudafactory@gmail.com>
2020-12-16 22:34:50 +0900

    使用しないクエリを削除

--- ListView/app/src/main/java/com/github/kazenetu/listview/room/TodoDao.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/room/TodoDao.kt
@@ -11,9 +11,6 @@ interface TodoDao {
     @Query("SELECT * FROM items WHERE isDone = 1 ORDER BY id DESC")
     fun getDone(): LiveData<List<TodoItem>>
 
-    @Query("SELECT * FROM items WHERE id IN (:todoIds)")
-    suspend fun loadAllByIds(todoIds: IntArray): List<TodoItem>
-
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(todo: TodoItem)
 
