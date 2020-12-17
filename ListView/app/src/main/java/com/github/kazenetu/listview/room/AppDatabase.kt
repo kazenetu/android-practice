0cb4a3be8411d0faa3d4d7ca33f1fab6f12fb071
kazenetu <rakudafactory@gmail.com>
2020-12-17 21:38:42 +0900

    ASの警告を解消

--- ListView/app/src/main/java/com/github/kazenetu/listview/room/AppDatabase.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/room/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.github.kazenetu.listview.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(TodoItem::class), version = 2, exportSchema = false)
+@Database(entities = [TodoItem::class], version = 2, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun todoDao(): TodoDao
 }
