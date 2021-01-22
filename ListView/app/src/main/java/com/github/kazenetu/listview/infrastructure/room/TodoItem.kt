8f1a61e23c0a177673b9c4bec67fb8240c7d147a
kazenetu <rakudafactory@gmail.com>
2021-01-22 23:41:45 +0900

    TodoItemInterfaceを継承

--- ListView/app/src/main/java/com/github/kazenetu/listview/infrastructure/room/TodoItem.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/infrastructure/room/TodoItem.kt
@@ -1,17 +1,18 @@
 package com.github.kazenetu.listview.infrastructure.room
 
 import androidx.room.*
+import com.github.kazenetu.listview.domain.interfaces.TodoItemInterface
 
 /**
  * リストアイテム
  */
 @Entity(tableName = "items")
 data class TodoItem(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @Ignore var showImage: Boolean,
-    var title: String,
-    var detail: String,
-    @ColumnInfo(defaultValue = "false") var isDone: Boolean
-){
-    constructor(id:Int,title: String,detail: String):this(id,false,title,detail,false)
+    @PrimaryKey(autoGenerate = true) override val id: Int,
+    @Ignore override var showImage: Boolean,
+    override var title: String,
+    override var detail: String,
+    @ColumnInfo(defaultValue = "false") override var isDone: Boolean
+): TodoItemInterface {
+    constructor(id: Int, title: String, detail: String) : this(id, false, title, detail, false)
 }
