6b3fe316fb35a56cd69fec7b81f9dfb17c4ee7e5
kazenetu <rakudafactory@gmail.com>
2021-01-30 22:31:45 +0900

    RepositoryInterfaceを継承する実装に変更

--- ListView/app/src/main/java/com/github/kazenetu/listview/infrastructure/TodoRepository.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/infrastructure/TodoRepository.kt
@@ -1,22 +1,23 @@
 package com.github.kazenetu.listview.infrastructure
 
 import com.github.kazenetu.listview.domain.interfaces.TodoItemInterface
+import com.github.kazenetu.listview.domain.interfaces.TodoRepositoryInterface
 import com.github.kazenetu.listview.infrastructure.room.TodoDao
 import com.github.kazenetu.listview.infrastructure.room.TodoItem
 
-class TodoRepository(private val todoDao: TodoDao) {
-    suspend fun todoData() = todoDao.getTodo() as List<TodoItemInterface>
-    suspend fun doneData() = todoDao.getDone() as List<TodoItemInterface>
+class TodoRepository(private val todoDao: TodoDao): TodoRepositoryInterface {
+    override suspend fun todoData() = todoDao.getTodo() as List<TodoItemInterface>
+    override suspend fun doneData() = todoDao.getDone() as List<TodoItemInterface>
 
-    suspend fun insert(todo: TodoItemInterface) {
+    override suspend fun insert(todo: TodoItemInterface) {
         todoDao.insert(TodoItem(todo))
     }
 
-    suspend fun update(todo: TodoItemInterface) {
+    override suspend fun update(todo: TodoItemInterface) {
         todoDao.update(TodoItem(todo))
     }
 
-    suspend fun delete(todo: TodoItemInterface) {
+    override suspend fun delete(todo: TodoItemInterface) {
         todoDao.delete(TodoItem(todo))
     }
 }
