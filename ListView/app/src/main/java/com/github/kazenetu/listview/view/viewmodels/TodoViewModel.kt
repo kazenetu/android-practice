d63168a79013623019dbc1105acf8bbf40309182
kazenetu <rakudafactory@gmail.com>
2021-02-24 23:25:45 +0900

    ローディング用タイマーイベントの追加

--- ListView/app/src/main/java/com/github/kazenetu/listview/view/viewmodels/TodoViewModel.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/view/viewmodels/TodoViewModel.kt
@@ -1,9 +1,11 @@
 package com.github.kazenetu.listview.view.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.github.kazenetu.listview.application.TodoApplicationService
 import com.github.kazenetu.listview.domain.domain.TodoEntity
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.launch
 
 /**
@@ -24,5 +26,25 @@ class TodoViewModel(applicationService: TodoApplicationService): ViewModel(appli
      */
     override suspend fun getSelectData():List<TodoEntity> {
         return applicationService.todoData()
+    }
+
+    /**
+     * Loadingの表示制御
+     */
+    private var loading:Boolean = true
+    private var firstSetTime:Boolean=true
+    override fun setTimeOut(ms :Long){
+        if(!loading) return
+
+        if(firstSetTime){
+            super.setTimeOut(ms)
+        }else{
+            super.setTimeOut(0L)
+        }
+        firstSetTime = false;
+        loading = false
+    }
+    fun clearTimeout(){
+        loading = true
     }
 }
