a2f72d663da2754116c9c44e184655b0fde1e372
kazenetu <rakudafactory@gmail.com>
2021-04-13 23:54:30 +0900

    メソッドコメント修正

--- ListView/app/src/main/java/com/github/kazenetu/listview/view/fragments/RecyclerViewFragment.kt
+++ ListView/app/src/main/java/com/github/kazenetu/listview/view/fragments/RecyclerViewFragment.kt
@@ -176,14 +176,8 @@ open class RecyclerViewFragment : Fragment() {
 
     companion object {
         /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecyclerViewFragment.
+         * インスタンス生成
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance() =
             RecyclerViewFragment().apply {
